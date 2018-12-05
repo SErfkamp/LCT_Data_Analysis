f0c7a825f92af92c25809df67a2fbb14a2638d20
Sönke Erfkamp <soenke.erfkamp@tum.de>
2018-12-05 02:50:06 +0100

    timeBetween files

---
+++ R Scripts/timeBetweenGlancesInteraction.R
@@ -0,0 +1,101 @@
+os <- "C:/Users/serfk/"
+#os <- "/Users/se/"
+
+files <- list.files(path=paste(os,"OneDrive/Thesis/Auswertung/Daten/Merged/",sep = ""), pattern="*.csv", full.names=T, recursive=FALSE)
+
+n <- 0
+sum <- 0
+min <- 99999
+minProband <- 0
+minEnd <- 0
+j<-0
+openGlance <- F
+previousInputTime <- 0
+previousEvent <- ""
+
+allValues <- c()
+
+for(i in 1:length(files)) {
+  
+  inputs <- read.csv(files[i], header=T, stringsAsFactors = FALSE, sep = ";")
+  if(ncol(inputs) == 1) {
+    inputs <- read.csv(files[i], header=T, stringsAsFactors = FALSE, sep = ",")
+  }
+  
+  proband <- strsplit(files[i],"/")[[1]][9]
+  proband <- as.numeric(strsplit(proband, "\\.")[[1]][1])
+  
+  for (row in 1:nrow(inputs)) {
+    
+    if(inputs[row,"Event"] == "Hand leaves steering wheel" 
+    | inputs[row,"Event"] == "Hand returns to steering wheel") {
+      next
+    }
+    
+    if(inputs[row,"Event"] == "Start") {
+      previousEvent = "Start"
+      openGlance <- F
+      previousInputTime <- 0
+      next
+    }
+    
+    if(inputs[row,"Event"] == "glance_start") {
+      previousEvent = "glance_start"
+      openGlance = T
+      next
+    }
+    
+    if(inputs[row,"Event"] == "glance_end") {
+      previousEvent = "glance_end"
+      openGlance = F
+      next
+    }
+    
+    if(inputs[row,"Event"] == "Input") {
+      
+      if(is.nan(inputs[row,"Milliseconds"]) || is.na(inputs[row,"Milliseconds"])) {
+        next
+      }
+      
+      if(!openGlance) next
+      
+      if(previousInputTime == 0) {
+        previousInputTime <- inputs[row,"Milliseconds"]
+        previousEvent = "Input"
+        next
+      } 
+      
+      if(previousEvent == "glance_start") {
+        timeBetweenInputs = inputs[row,"Milliseconds"] - previousInputTime
+        if(timeBetweenInputs < 3000) {
+          allValues <- c(allValues, timeBetweenInputs)
+          
+        }
+        previousInputTime <- inputs[row,"Milliseconds"]
+        
+        sum <- sum + timeBetweenInputs
+        n <- n+1
+        
+        if(timeBetweenInputs < min) {
+          min <- timeBetweenInputs
+          minProband <- proband
+          minEnd <- previousInputTime
+        }
+        
+        print(paste("Proband", proband, "tBInputs", timeBetweenInputs))
+      
+      }
+      
+      previousEvent = "Input"
+      previousInputTime <- inputs[row,"Milliseconds"]
+      #print(paste("Proband", proband, "tBInputs", timeBetweenInputs, "sum", sum, "n", n))
+    }
+  }
+}
+
+average = sum / n
+
+qts <- quantile(allValues,probs=.10)
+hist(allValues, main = "Time between Inputs with glance on street inbetween")
+
+abline(v=qts[1],col="red")
---
+++ R Scripts/timeBetweenInputs.R
@@ -0,0 +1,86 @@
+os <- "C:/Users/serfk/"
+#os <- "/Users/se/"
+
+files <- list.files(path=paste(os,"OneDrive/Thesis/Auswertung/Daten/Merged/",sep = ""), pattern="*.csv", full.names=T, recursive=FALSE)
+
+n <- 0
+sum <- 0
+min <- 0
+max <- 0
+maxProband <- 0
+maxStart <- 0
+maxEnd <- 0
+j<-0
+openGlance <- F
+previousInputTime <- 0
+
+allValues <- c()
+
+for(i in 1:length(files)) {
+
+  inputs <- read.csv(files[i], header=T, stringsAsFactors = FALSE, sep = ";")
+  if(ncol(inputs) == 1) {
+    inputs <- read.csv(files[i], header=T, stringsAsFactors = FALSE, sep = ",")
+  }
+  
+  proband <- strsplit(files[i],"/")[[1]][9]
+  proband <- as.numeric(strsplit(proband, "\\.")[[1]][1])
+  
+  for (row in 1:nrow(inputs)) {
+
+    if(inputs[row,"Event"] == "Start" 
+       | inputs[row,"Event"] == "Hand leaves steering wheel" 
+       | inputs[row,"Event"] == "Hand returns to steering wheel") {
+      openGlance <- F
+      previousInputTime <- 0
+      next
+    }
+    
+    if(inputs[row,"Event"] == "glance_start") {
+      openGlance = T
+      next
+    }
+    
+    if(inputs[row,"Event"] == "glance_end") {
+      openGlance = F
+      previousInputTime <- 0
+      next
+    }
+    
+    if(inputs[row,"Event"] == "Input") {
+      
+      if(!openGlance) next
+      
+      if(is.nan(inputs[row,"Milliseconds"]) || is.na(inputs[row,"Milliseconds"])) {
+        next
+      }
+      
+      if(previousInputTime == 0) {
+        previousInputTime <- inputs[row,"Milliseconds"]
+        next
+      }
+      
+      timeBetweenInputs = inputs[row,"Milliseconds"] - previousInputTime
+      allValues <- c(allValues, timeBetweenInputs)
+      previousInputTime <- inputs[row,"Milliseconds"]
+
+      sum <- sum + timeBetweenInputs
+      n <- n+1
+      
+      print(paste("Proband", proband, "tBInputs", timeBetweenInputs, "sum", sum, "n", n))
+      
+      if(timeBetweenInputs > max) {
+        max <- timeBetweenInputs
+        maxProband <- proband
+        maxEnd <- previousInputTime
+      }
+    }
+  }
+}
+
+average = sum / n
+
+qts <- quantile(allValues,probs=.90)
+hist(allValues,main = "Time between Inputs while looking on the IVIS")
+
+abline(v=qts[1],col="red")
